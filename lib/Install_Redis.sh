5dfe156ff94093ff1ac6973d85eaa4e71f200fa3
fox <fox.wang@eg-global.cn>
2015-11-13 10:04:59 +0800

    redis

--- lib/Install_Redis.sh
+++ lib/Install_Redis.sh
@@ -8,10 +8,24 @@ function Install_Redis()
 
 
 	local Memcached_DIR=$IN_DIR/memcached
-	wget ${DUS['redis']}
-	tar xzf redis-${VERS['redis']}.tar.gz
-	cd redis-${VERS['redis']}
+	phpRedisFile=$IN_DOWN/redis-stable.tar.gz
+	if [ ! -f "$phpRedisFile" ]; then
+	    wget ${DUS['redis']} -O redis-stable.tar.gz
+	fi
+	tar zxvf redis-stable.tar.gz
+	cd redis-stable
 	make PREFIX=$IN_DIR/redis install
+	cp redis.conf $IN_DIR/redis/redis.conf
+	sed -i "s:daemonize no:daemonize yes:g" $IN_DIR/redis/redis.conf
+	sed -i "s:timeout 0:timeout 300:g" $IN_DIR/redis/redis.conf
+	sed -i "s:loglevel notice:loglevel debug:g" $IN_DIR/redis/redis.conf
+	sed -i "s:logfile """":logfile ${IN_WEB_LOG_DIR}/redis.log:g" $IN_DIR/redis/redis.conf
+	
+	cp ./utils/redis_init_script $IN_DIR/action/redis
+	sed -i "s:EXEC=/usr/local/bin/redis-server:EXEC=${IN_DIR}/redis/bin/redis-server:g" $IN_DIR/action/redis
+	sed -i "s:CLIEXEC=/usr/local/bin/redis-cli:CLIEXEC=${IN_DIR}/redis/bin/redis-cli:g" $IN_DIR/action/redis
+	sed -i "s:/etc/redis/${REDISPORT}.conf:${IN_DIR}/redis/redis.conf:g" $IN_DIR/action/redis
+	sed -i "s:/var/run/redis_${REDISPORT}.pid:/var/run/redis.pid:g" $IN_DIR/action/redis
 
 
 	echo "=========================== install Redis  END ======================"
