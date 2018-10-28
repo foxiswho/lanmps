cc75bb3e8945e0463c3a83b0a9ce6d03d8febe8d
fox <80005368@qq.com>
2018-10-28 17:32:34 +0800

    增加 nginx http2模块支持

--- lib/Install_Nginx.sh
+++ lib/Install_Nginx.sh
@@ -17,6 +17,7 @@ function Install_Nginx {
 	--group=www \
 	--prefix=$IN_DIR/nginx \
 	--with-http_stub_status_module \
+	--with-http_v2_module \
 	--with-http_ssl_module \
 	--with-http_gzip_static_module \
 	--with-http_secure_link_module \
